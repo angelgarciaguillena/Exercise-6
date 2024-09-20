5e7cebd65a6ace4240c87393cc148653b9b00981
angel.garcia <angel.garcia@106-11.info2>
2024-09-20 14:25:48 +0200

    Exercise 6 realizado

---
+++ src/exercise6/Exercise6.java
@@ -0,0 +1,11 @@
+package exercise6;
+
+public class Exercise6 {
+	public static void main(String[] args) {
+		System.out.println("Ángel García Guillena");
+		System.out.println("angel.garcia@iesnervion.es");
+		System.out.println("");
+		System.out.println("641280553");
+	}
+}
+
